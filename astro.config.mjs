13afe4808e857cfd1cf6ea912e2dfaba24f79dfd
goopscoop <codybarrus@gmail.com>
2026-06-04 19:55:13 -0700

    updated site value

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://therefuge.scbarrus.com',
   integrations: [react(), mdx()],
   vite: {
     plugins: [tailwindcss()],
